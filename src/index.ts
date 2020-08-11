842923ad3fb7f29e33104c669ae95abf070213b6
Michele Cantelli <emmekappa@gmail.com>
2020-08-11 19:42:18 +0200

    disabling upload component

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,8 @@ const createWindow = () => {
     // and load the index.html of the app.
     mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    if(!app.isPackaged)
+        mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
