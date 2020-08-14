0afe550532dbae08494b43c26275f563c2d0a741
Michele Cantelli <emmekappa@gmail.com>
2020-08-14 19:07:48 +0200

    olè

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,11 +1,5 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = [
-  new ForkTsCheckerWebpackPlugin(),
-  new CopyPlugin({
-    patterns: [
-      { from: './node_modules/sharp/', to: './output/node_modules/sharp/' },
-    ],
-  }),
+  new ForkTsCheckerWebpackPlugin()
 ];
