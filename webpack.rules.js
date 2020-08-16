f7352418a355d870d0769e329431f022cb0562fe
Michele Cantelli <emmekappa@gmail.com>
2020-08-16 18:55:57 +0200

    Revert "Trying to implementing fix as described https://github.com/electron-userland/electron-forge/issues/1688#issuecomment-666535365"
    This reverts commit e0c62aec

--- webpack.rules.js
+++ webpack.rules.js
@@ -1,29 +1,28 @@
-const in_packager = process.env.npm_lifecycle_script.search(/package/) >= 0;
-
 module.exports = [
-    (!in_packager ? {
-            test: /\.node$/,
-            use: 'node-loader',
-        } :
-        {
-            test: /\.(m?js|node)$/,
-            parser: {amd: false},
-            use: {
-                loader: '@marshallofsound/webpack-asset-relocator-loader',
-                options: {
-                    outputAssetBase: 'native_modules',
-                },
-            },
-        }),
-    {
-        test: /\.tsx?$/,
-        exclude: /(node_modules|\.webpack)/,
-        use: {
-            loader: 'ts-loader',
-            options: {
-                transpileOnly: true
-            }
-        }
+  // Add support for native node modules
+  {
+    test: /\.node$/,
+    use: 'node-loader',
+  },
+  {
+    test: /\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      options: {
+        outputAssetBase: 'native_modules',
+      },
     },
-    {test: /\.scss$/, loaders: ['style', 'css', 'sass']},
+  },
+  {
+    test: /\.tsx?$/,
+    exclude: /(node_modules|\.webpack)/,
+    use: {
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true
+      }
+    }
+  },
+  { test: /\.scss$/, loaders: ['style', 'css', 'sass']},
 ];
