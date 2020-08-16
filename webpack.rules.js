e0c62aece2cc90e30b35f6903053af5227b18a0f
Michele Cantelli <emmekappa@gmail.com>
2020-08-16 18:50:01 +0200

    Trying to implementing fix as described https://github.com/electron-userland/electron-forge/issues/1688#issuecomment-666535365

--- webpack.rules.js
+++ webpack.rules.js
@@ -1,28 +1,29 @@
+const in_packager = process.env.npm_lifecycle_script.search(/package/) >= 0;
+
 module.exports = [
-  // Add support for native node modules
-  {
-    test: /\.node$/,
-    use: 'node-loader',
-  },
-  {
-    test: /\.(m?js|node)$/,
-    parser: { amd: false },
-    use: {
-      loader: '@marshallofsound/webpack-asset-relocator-loader',
-      options: {
-        outputAssetBase: 'native_modules',
-      },
+    (!in_packager ? {
+            test: /\.node$/,
+            use: 'node-loader',
+        } :
+        {
+            test: /\.(m?js|node)$/,
+            parser: {amd: false},
+            use: {
+                loader: '@marshallofsound/webpack-asset-relocator-loader',
+                options: {
+                    outputAssetBase: 'native_modules',
+                },
+            },
+        }),
+    {
+        test: /\.tsx?$/,
+        exclude: /(node_modules|\.webpack)/,
+        use: {
+            loader: 'ts-loader',
+            options: {
+                transpileOnly: true
+            }
+        }
     },
-  },
-  {
-    test: /\.tsx?$/,
-    exclude: /(node_modules|\.webpack)/,
-    use: {
-      loader: 'ts-loader',
-      options: {
-        transpileOnly: true
-      }
-    }
-  },
-  { test: /\.scss$/, loaders: ['style', 'css', 'sass']},
+    {test: /\.scss$/, loaders: ['style', 'css', 'sass']},
 ];
